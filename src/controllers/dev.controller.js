96556596c52e774e467334b5e8bdbd8c9074011c
Mostafa Ramadan <mo_star44@hotmail.com>
2021-10-14 17:19:49 +0200

    feature: generate token

--- src/controllers/dev.controller.js
+++ src/controllers/dev.controller.js
@@ -1,11 +1,10 @@
 const secretKey = require('secret-key');
+const {StatusCodes} = require("http-status-codes");
 const { createApp } = require("../models/User");
 const { BAD_REQUEST, INTERNAL_SERVER_ERROR } = require("../utils/responses");
 const {ServerError} = require("../utils/error-handler");
 
-const registerApp = async (req,res) => {
-    const appName = req.body.appname;
-    const filter = {username: req.body.username}
+const generatePassphrase = ()=>{
     const passphraseLen = 28;
     let passphrase = "";
     const validChars = [];
@@ -24,44 +23,41 @@ const registerApp = async (req,res) => {
         randomNum = Math.ceil(randomNum);
         passphrase += validChars[randomNum];
     }
+    return passphrase;
+};
 
+const registerApp = async (req,res) => {
+    const passphrase = generatePassphrase();
     const keyDetails = secretKey.create(passphrase);
-    const {secret, iv, timestamp } = keyDetails;
-    // .secret, keyDetails.iv, keyDetails.timestamp;
-    // const iv = ;
-    // const timestamp = ;
+    const { secret, iv, timestamp } = keyDetails;
 
     try {
+        const name = req?.body?.appname;
+        const filter = {username: req?.body?.username};
+        if (name === null || name === undefined)
+            return BAD_REQUEST(res,"Missing app name");
+
         const newApp = {
             $push: {
                 apps: {
-                    appName,
+                    name,
                     iv,
                     timestamp,
                     passphrase
                 }
             }
         };
+
         const result = await createApp(filter, newApp);
-        if (result === null || typeof  result === "undefined")
+        if (result === null || typeof result === "undefined")
             return BAD_REQUEST(res,"Could not add app");
+
+        const response = {name, secret};
+        return res.status(StatusCodes.CREATED).json(response);
     }catch (error) {
         if (error instanceof ServerError) return INTERNAL_SERVER_ERROR(res);
         return res.send(error);
     }
-  return res.send(`added app with name: ${appName}, 
-  with secretkey:${secret}, please save the key as you won't have access to it again`);
 };
-
-// const useApp = async (req,res){
-//     let username = req.body.username;
-//     let appName = req.body.appName;
-//     let secret = req.body.secret;
-//     let userFilter = {username: req.body.username};
-//     let appFilter = {name:appName};
-//     findApp(userfilter,appFilter);
-//     secretKey.check(passphrase,secret,iv,timestamp);
-//     // make a transaction in app
-// }
 
 module.exports = { registerApp };
